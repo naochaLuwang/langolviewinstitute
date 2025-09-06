a001719e6110412cb6e3e0f41faeec9cd35ca7d8
Somojit Arambam <somoarambam123@gmail.com>
2025-09-06 16:51:18 +0530

    Speed Insights added

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 
 import './globals.css';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function RootLayout({ children }) {
   return (
@@ -7,6 +8,7 @@ export default function RootLayout({ children }) {
       <body>
 
         {children}
+        <SpeedInsights />
 
       </body>
 
